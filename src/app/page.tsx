b3c19cfb222058b59575f94ef64dc1168b405cd9
Eryk-Kanownik <eryk.patryk.kanownik@gmail.com>
2023-10-01 12:09:13 +0200

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,6 @@
+import Image from "next/image";
+import styles from "../styles/page-styles/page.module.scss";
+
+export default function Home() {
+  return <main className={styles.index}></main>;
+}
